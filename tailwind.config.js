90022ac2591bcb6a24b69ce2c79c4e345ce2dac5
Aravind Balla <bsaaravind@gmail.com>
2022-06-23 10:58:40 +0530

    feat: upgrade tailwind + more colors 🎨

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  // purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    { pattern: /bg-gradient-to-.*/ },
+    {
+      pattern: /(from|to)-.*/,
+    },
+  ],
   darkMode: "class", // 'media' or 'class'
   theme: {
     extend: {
